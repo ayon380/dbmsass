0dec0e4417c2525c9598179a2c1f2627eb82c6c1
Ayon Sarkar <ayonsarkar380@gmail.com>
2023-11-18 00:56:58 +0530

    v1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,84 @@
-"use client"
+// Import React and other necessary modules
+"use client" 
 import React from 'react';
-import BookForm from '../components/BookForm'
+import BookForm from '../components/BookForm';
 import EcomForm from '../components/EcomForm';
-import EcomGet from '../components/EcomGet'
-import BookGet from '../components/BookGet'
+import EcomGet from '../components/EcomGet';
+import BookGet from '../components/BookGet';
+
 export default function Home() {
   const [page, setPage] = React.useState("");
   const [books, setBooks] = React.useState([]);
   const [products, setProducts] = React.useState([]);
+
   const getBooks = async () => {
-    const response = await fetch('/api/library/get', { cache: 'no-store' });
-    const res = await response.json();
-    setBooks(res.data);
-    // console.log(res.data);
+    try {
+      const response = await fetch('/api/library/get', { cache: 'no-store' });
+      const data = await response.json();
 
-  }
+      if (response.ok) {
+        setBooks(data.data);
+      } else {
+        console.error('Error fetching books:', data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching books:');
+    }
+  };
+
   const getProducts = async () => {
-    const response = await fetch('/api/ecom/get', { cache: 'no-store' });
-    const res = await response.json();
-    setProducts(res.data);
-    console.log(res.data);
+    try {
+      const response = await fetch('/api/ecom/get', { cache: 'no-store' });
+      const data = await response.json();
 
-  }
+      if (response.ok) {
+        setProducts(data.data);
+      } else {
+        console.error('Error fetching products:', data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching products:');
+    }
+  };
+
   React.useEffect(() => {
     getBooks();
     getProducts();
   }, []);
+
   return (
-    <main>
-      <div>
-        <button className='' onClick={() => { setPage("book") }}>Book</button>
-        <button onClick={() => { setPage("ecom") }}>Ecom</button></div>
-      {page === "book" ? <div> <BookForm getBooks={getBooks} />
-        <BookGet books={books} /></div> : null}
-      {page === "ecom" ? <div><EcomForm getProducts={getProducts} /><EcomGet products={products} /></div> : null}
+    <main className="container mx-auto my-8 p-4 relative">
+      <h1 className="text-3xl font-bold text-center mb-8 relative z-10 ">
+        <span className="flowing-background">DBMS Assignment</span>
+      </h1>
+      <div className="flex justify-around mb-8">
+        <button
+          className={`px-4 py-2 rounded-full transition duration-300 ease-in-out ${page === 'book' ? 'bg-blue-500 text-white' : 'bg-gray-300 hover:bg-blue-300 hover:text-white'}`}
+          onClick={() => setPage('book')}
+        >
+          Book
+        </button>
+        <button
+          className={`px-4 py-2 rounded-full transition duration-300 ease-in-out ${page === 'ecom' ? 'bg-blue-500 text-white' : 'bg-gray-300 hover:bg-blue-300 hover:text-white'}`}
+          onClick={() => setPage('ecom')}
+        >
+          Ecom
+        </button>
+      </div>
+
+      {page === 'book' ? (
+        <div>
+          <BookForm getBooks={getBooks} />
+          <BookGet books={books} />
+        </div>
+      ) : null}
+
+      {page === 'ecom' ? (
+        <div>
+          <EcomForm getProducts={getProducts} />
+          <EcomGet products={products} />
+        </div>
+      ) : null}
     </main>
-  )
+  );
 }
